34edce7338c11f86a140d92ee3977aa3600e2597
IsratJahanR <isratjahan151q@gmail.com>
2025-01-23 11:33:55 +0600

    add requirements.txt

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "hello world"
+    return "<h1>hello world</h1>"
 
 @app.route("/about")
 def about():
